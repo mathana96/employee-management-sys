661859c93d8837558527e926e8a2ffc250e21fca
Mathana Nair <mathana96@gmail.com>
2016-04-15 19:20:04 +0100

    Added extra validations

--- src/Driver.java
+++ src/Driver.java
@@ -7,7 +7,7 @@ public class Driver
 {
 	private Scanner	input;
 	private List<Employee> employees;
-
+	private List<Employee> managers; 
 
 	public static void main(String[] args)
 	{
@@ -19,6 +19,7 @@ public class Driver
 	{
 		input = new Scanner(System.in);
 		employees = new ArrayList<Employee>();
+		managers = new ArrayList<Employee>();
 	}
 
 	public int mainMenu()
@@ -26,14 +27,15 @@ public class Driver
 		
 		int option = 0;
 		boolean errorFree = false;
-		do
+		while (!errorFree)
 		{
 			try
 			{
+				System.out.println("\n============= MAIN MENU =============\n");
 				System.out.println("1)	Add an employee");
 				System.out.println("2)	List employees");
-				System.out.println("3)	Add employee to a department");
-				System.out.println("4)	Employee details");
+				System.out.println("3)	Assign employee to a department");
+				System.out.println("4)	Calculate salary");
 				System.out.println("\n0)	Exit system");
 				option = input.nextInt();
 				errorFree = true;
@@ -45,7 +47,7 @@ public class Driver
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
 			
-		} while (!errorFree);
+		}
 		
 		System.out.println(option);
 		return option;
@@ -68,7 +70,6 @@ public class Driver
 				break;
 				
 			case 3:
-//				System.out.println(listManagers());
 				addToDept();
 				break;
 
@@ -78,7 +79,7 @@ public class Driver
 			// break;
 
 			default:
-				System.out.println("Invalid option selected");
+				System.out.println("Invalid option. Please try again.");
 			}
 			option = mainMenu();
 		}
@@ -90,10 +91,11 @@ public class Driver
 	{
 		int option = 0;
 		boolean errorFree = false;
-		do
+		while (!errorFree)
 		{
 			try
 			{
+				System.out.println("\n============= WORKER MENU =============\n");
 				System.out.println("Please select which type of employee you would like to add");
 				System.out.println("1)	Manager");
 				System.out.println("2)	Sales worker");
@@ -108,7 +110,6 @@ public class Driver
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
 		}
-		while (!errorFree);
 		return option;		
 	}
 	
@@ -136,7 +137,7 @@ public class Driver
 				break;
 
 			default:
-				System.out.println("Invalid option\n");
+				System.out.println("Invalid option. Please try again.\n");
 				option = workerMenu();
 			}		
 		}
@@ -154,7 +155,7 @@ public class Driver
 
 		double hourlyRate = 0.0;
 		boolean errorFree = false;
-		do
+		while (!errorFree)
 		{
 			try
 			{
@@ -167,11 +168,11 @@ public class Driver
 				input.nextLine();
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
-		}	while (!errorFree);	
+		}	
 		
 		double bonus = 0.0;
 	  errorFree = false;
-		do
+	  while (!errorFree)
 		{
 			try
 			{
@@ -185,7 +186,7 @@ public class Driver
 				input.nextLine();
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
-		} while (!errorFree);
+		}
 
 		employees.add(new Manager(firstName, secondName, hourlyRate, bonus));
 
@@ -202,7 +203,7 @@ public class Driver
 
 		double hourlyRate = 0.0;
 		boolean errorFree = false;
-		do
+		while (!errorFree)
 		{
 			try
 			{
@@ -215,15 +216,15 @@ public class Driver
 				input.nextLine();
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
-		}	while (!errorFree);	
+		}
 		
 		double bonus = 0.0;
 	  errorFree = false;
-		do
+	  while (!errorFree)
 		{
 			try
 			{
-    		System.out.println("Sales bonus: ");
+    		System.out.println("Sales bonus percentage (only values between 0 and 20): ");
     		bonus = input.nextDouble();
     		errorFree = true;
 
@@ -233,7 +234,7 @@ public class Driver
 				input.nextLine();
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
-		} while (!errorFree);
+		} 
 
 		employees.add(new SalesWorker(firstName, secondName, hourlyRate, bonus));
 
@@ -250,7 +251,7 @@ public class Driver
 
 		double hourlyRate = 0.0;
 		boolean errorFree = false;
-		do
+		while (!errorFree)
 		{
 			try
 			{
@@ -263,7 +264,7 @@ public class Driver
 				input.nextLine();
 				System.out.println("\nOnly numeric values accepted. Please try again.\n");
 			}
-		}	while (!errorFree);	
+		}	
 
 		employees.add(new TempWorker(firstName, secondName, hourlyRate));
 
@@ -271,30 +272,90 @@ public class Driver
 
 	public void addToDept()
 	{
-		System.out.println(listAll());
-		System.out.println("Please select employee (Employee ID)");
-		int selectedEmployee = input.nextInt() - 1;
-		
-		Employee employee = employees.get(selectedEmployee);
-		
-		boolean isManager = false;
-		
-		while (!isManager)
+		if (!listManagers().isEmpty())
 		{
-			input.nextLine();
-			System.out.println(listManagers());
-			System.out.println("Please select the manager whose department you wish to assign the employee to");
-			
-			int selectedManager = input.nextInt() - 1;
-			Employee emp = employees.get(selectedManager);
-			
-			if (emp.getClass().equals(Manager.class))
-			{
-				Manager man = (Manager) employees.get(selectedManager);
-				man.addDeptEmployee(employee);
-				isManager = true;
-			}
-			
+    		System.out.println(listAll());
+    		int selectedEmployee = 0;
+    		int selectedManager = 0;
+    		boolean errorFree = false;
+    		while (!errorFree)
+    		{
+    			try
+    			{
+        		System.out.println("Please select employee (Employee ID): ");
+        		selectedEmployee = input.nextInt() - 1;
+        		if (selectedEmployee < employees.size())
+        		{
+        			
+        			if (employees.get(selectedEmployee).getClass().equals(Manager.class))
+        			{
+        				System.out.println("Cannot add Manager to department. Please try again.\n");
+        			}
+        			else
+        			{
+        				errorFree = true;
+        			}
+        		}	
+        		else
+        		{
+        			System.out.println("Invalid option. Please try again. ");
+        		}
+    			}	
+    			catch (Exception e)
+    			{
+    				input.nextLine();
+    				System.out.println("\nOnly numeric values accepted. Please try again.\n");
+    			}
+    			
+    		}		
+    		Employee employee = employees.get(selectedEmployee);
+    		
+    		errorFree = false;
+    		boolean isManager = false;
+    		
+    		while (!isManager)
+    		{
+    			while (!errorFree)
+    			{
+    				try
+    				{
+        			input.nextLine();
+        			System.out.println("\n============= MANAGER LIST =============");
+        			System.out.println(listManagers());
+        			System.out.println("Please select the manager whose department you wish to assign the employee to: ");
+        		  selectedManager = input.nextInt() - 1;
+        		  if (selectedManager < employees.size())
+          		{
+          			errorFree = true;
+          		}	
+          		else
+          		{
+          			System.out.println("Invalid option. Please try again. ");
+          		}
+    				}
+    				catch (Exception e)
+    				{
+    					System.out.println("\nOnly numeric values accepted. Please try again.\n");
+    				}
+    			}	
+    			Employee emp = employees.get(selectedManager);
+    			
+    			if (emp.getClass().equals(Manager.class))
+    			{
+    				Manager man = (Manager) employees.get(selectedManager);
+    				man.addDeptEmployee(employee);
+    				isManager = true;
+    			}
+    			else
+    			{
+    				System.out.println("Invalid option. Please try again. ");
+    				errorFree = false;
+    			}
+    		}
+		}
+		else
+		{
+			System.out.println("\nExisting managers not found. At least 1 manager required to add employee to department\n");
 		}
 	}
 
@@ -302,24 +363,35 @@ public class Driver
 	{
 		String list = "";
 		int i = 0;
-		
-		for (Employee employee : employees)
+		list += "\n============= EMPLOYEE LIST =============\n";
+		if (employees.size() > 0)
 		{
-			list += "\n===============================\nWorker ID: " + (i+1) + employee.toString() + "\n===============================\n"; 	
-			i++;
+			for (Employee employee : employees)
+			{
+				
+				list += "\n===============================\nWorker ID: " + (i+1) + employee.toString() + "\n===============================\n"; 	
+				i++;
+			}
+			
+		}
+		else
+		{
+			list += "\nNo employees\n";
 		}
 		return list;
 	}	
 	
-		public String listManagers() 
+	public String listManagers() 
 		{
 			String list = "";
 			int i = 0;
 			
 			for (Employee employee : employees)
 			{
+				
 				if (employee.getClass().equals(Manager.class))
 				{
+					
 					list += "\n===============================\nWorker ID: " + (i+1) + 
 							    "\nPosition: Manager" + 
 							    "\n" + "First name: " + employee.getFirstName() + 
